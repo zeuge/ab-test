9d4f96846fef6ea4f5e97bf0135c819bddf65dcf
Eugene Zhukov <zeuge74@gmail.com>
2016-02-22 14:43:12 +0500

    add bootstrap-sass

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,7 +17,7 @@ class PagesController < ApplicationController
     if @page.save
       redirect_to page_url(@page), notice: 'Page was successfully created.'
     else
-      flash.now[:alert] = @page.errors.full_messages.join("; ")
+      flash.now[:alert] = @page.errors.full_messages.join '; '
       render :new
     end
   end
@@ -29,7 +29,7 @@ class PagesController < ApplicationController
     if @page.update page_params
       redirect_to page_url(@page), notice: 'Page was successfully updated.'
     else
-      flash.now[:alert] = @page.errors.full_messages.join("; ")
+      flash.now[:alert] = @page.errors.full_messages.join '; '
       render :edit
     end
   end
@@ -46,7 +46,7 @@ class PagesController < ApplicationController
   end
 
   def page_params
-    params.require(:page).permit(:name, :title, :body)
+    params.require(:page).permit :name, :title, :body
   end
 
 end
